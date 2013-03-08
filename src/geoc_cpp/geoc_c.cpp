65864b381c21be34f49b05c7c57bb26efe75aa56
Martin Landa <martin.landa@fsv.cvut.cz>
2013-03-08 14:35:21 +0100

    empty files for capi

---
+++ src/geoc_cpp/geoc_c.cpp
@@ -0,0 +1,5 @@
+#include "geoc.h"
+#include "geoc_c.h"
+
+extern "C" {
+}
---
+++ src/geoc_cpp/geoc_c.h
@@ -0,0 +1,16 @@
+#ifndef GEOC_C_H
+#define GEOC_C_H
+
+#include "geos_c.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  // todo
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // GEOC_C_H
