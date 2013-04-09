81dce6b8fa2ef5751ea50944e93d518c4621e530
Tereza <tfiedlerova@gmail.com>
2013-04-09 14:22:26 +0200

    nahran soubor sort.h pro razeni bodu

---
+++ src/geoc_cpp/sort.h
@@ -0,0 +1,49 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include "geoc.h"
+
+#include <cmath>
+
+/** Class for sort Coordinate by X */
+
+class SortByX
+{
+    public:
+
+        bool operator() (const Coordinate& p1, const Coordinate& p2) const
+        {
+            const double TOL = 1e-14;
+
+            if ( std::abs( p1.x-p2.x ) < TOL)
+            {
+                return p1.y < p2.y;
+            }
+
+            return p1.x < p2.x;
+        }
+};
+
+
+/** Class for sort Coordinate by Y */
+
+class SortByY
+{
+    public:
+
+        bool operator() (const Coordinate& p1, const Coordinate& p2) const
+        {
+            const double TOL = 1e-14;
+
+            if ( std::abs( p1.y-p2.y ) < TOL)
+            {
+                return p1.x < p2.x;
+            }
+
+            return p1.y < p2.y;
+        }
+};
+
+
+
+#endif // SORT_H
